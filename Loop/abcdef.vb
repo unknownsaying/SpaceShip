1447e1ced83bb0034292fb03eaec81975d1ea52c
Visual Basic .net <15368104568@189.cn>
2025-10-10 11:45:17 +0800

    Update abcdef.vb

--- Loop/abcdef.vb
+++ Loop/abcdef.vb
@@ -1,6 +1,6 @@
-a + b + c + d + e + f 
-a - b - c - d - e - f
-a * b * c * d * e * f
-a / b / c / d / e / f
-a % b % c % d % e % f
-a : b : c : d : e : f
+Dim a as 1,3,5,7,9
+Dim b as 2,4,6,8,0
+Redim c as 1/2,3/4,5/6,7/8,9/0
+Redim d as 2/1,4/3,6/5,8/7,0/9
+Dim e as AddHandler
+ReDim f as RemoveHandler
