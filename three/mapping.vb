8c4b3796adc92981a0ae81303db3a3fc47e78e63
Visual Basic .net <15368104568@189.cn>
2025-10-10 11:50:47 +0800

    Update mapping.vb

--- three/mapping.vb
+++ three/mapping.vb
@@ -4,5 +4,5 @@ return e % exp(E) + f % log(F) + g % exp(G) + h % log(H)
 return i / asin(I) + j / acos(J) + k / asin(K) + l / acos(L)
 return m : sinh(M) + n : cosh(N) + o : sinh(O) + p : cosh(P)
 return r ^ asinh(R) + s ^ acosh(S) + t ^ asinh(T) + uv ^ acosh(UV)
-return w * tan(W) + x * atan(X) + y * tan(Y)+ z * atan(Z)
+return w * tan(W) + x * atan(X) + y * tan(Y) + z * atan(Z)
 End Select
