74faa11d1905230ab6701bc613d83966e257f983
Visual Basic .net <15368104568@189.cn>
2025-10-11 13:00:47 +0800

    Update DoubleCrak.vb

--- Loop/DoubleCrak.vb
+++ Loop/DoubleCrak.vb
@@ -25,17 +25,17 @@ for(orbifold=0;orbifold>=8;++orbifold)
 End Select
 
 Select 963
-For(A=0;A<=2;++A)
-For(B=00;-2<=B&&B<=2;++B||--B)
-For(C=000;C>=2;--C)
+for(A=0;A<=2;++A)
+for(B=00;-2<=B&&B<=2;++B||--B)
+for(C=000;C>=2;--C)
 
-For(D=0000;D<=4;D++)
-For(E=0000;-4<=E4;E++)
-For(for48=0000;for369;for84)
+for(D=0000;D<=4;D++)
+for(E=0000;-4<=E4;E++)
+for(for48=0000;for369;for84)
 
-For(F=000;F>=8;F--)
-For(MAniFolD=00;-8<=MAniFolD&&o<=MAniFolD;++ABCDEF||--ABCDEF)
-For(orBiFolD=0;orBiFolD>=8;++orBiFolD)
+for(F=000;F>=8;F--)
+for(MAniFolD=00;-8<=MAniFolD&&o<=MAniFolD;++ABCDEF||--ABCDEF)
+for(orBiFolD=0;orBiFolD>=8;++orBiFolD)
 End Select
 
 Select 84
