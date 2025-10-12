7e66295a65b7d554ca1d38fc78c61dbe74f74954
unknownsaying <15368104568@189.cn>
2025-10-12 20:03:37 +0800

    Update abcdef.vb

--- Loop/abcdef.vb
+++ Loop/abcdef.vb
@@ -4,3 +4,9 @@ dim c as 1/2,3/4,5/6,7/8,9/0
 dim d as 2/1,4/3,6/5,8/7,0/9
 ReDim e as AddHandler
 ReDim f as RemoveHandler
+a + b + c + d + e + f
+a - b - c - d - e - f
+a / b / c / d / e / f
+a * b * c * d * e * f
+a % b % c % d % e % f
+a ^ b ^ c ^ d ^ e ^ f
