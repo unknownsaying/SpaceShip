898c1ffc45f934fdbbb368d77ae6265e08661335
unknownsaying <15368104568@189.cn>
2025-10-17 07:42:01 +0800

    Update coordinate.c

--- Universe/coordinate.c
+++ Universe/coordinate.c
@@ -5,6 +5,7 @@ void sphere(){
     typedef Z ; radius * cos(theta) * sin(fai);
     typedef T ; radius * cos(theta) * cos(fai);
  }
+
 void loop(){
     char a,b,c,d,e,f;
    for(a=0;a<b;a++)
@@ -13,4 +14,20 @@ void loop(){
    for(d=000;d>e;--d)
    for(e=00;e>f;--e)
    for(f=0;f>a;--f)
+;}
+
+void sphere(){
+     int X,Y,Z,T,r;
+     char theta,fai;
+     X = r * sin(theta) * cos(fai);
+     Y = r * sin(theta) * sin(fai);
+     Z = r * cos(fai);
+     T = r * sin(theta);
+
+  const Length = 1; 
+  const r = Length;
+    Coordinate(0,0,1),//A
+    Coordinate(acos(-1/3), 0,0), // B
+    Coordinate(0, -acos(-1/3),0), // C
+    Coordinate(0,0,acos(-1/3)) // D
 ;}
