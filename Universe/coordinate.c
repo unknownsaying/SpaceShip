4a8fa7323377f7479408f31120fd4a8f3460de35
unknownsaying <15368104568@189.cn>
2025-10-17 08:19:23 +0800

    Update coordinate.c

--- Universe/coordinate.c
+++ Universe/coordinate.c
@@ -1,9 +1,10 @@
-void sphere(){    
-    char radius,theta,fai;
-    typedef X ; radius ;
-    typedef Y ; radius * cos(theta);
-    typedef Z ; radius * cos(theta) * sin(fai);
-    typedef T ; radius * cos(theta) * cos(fai);
+#define c 299792458 lightspeed
+void sphere(int radius,float theta,double fai){    
+    char radius, theta, fai;
+    typedef X ; radius * sin(theta) * cos(fai);
+    typedef Y ; radius * sin(theta) * sin(fai);
+    typedef Z ; radius * cos(theta) ;
+    this.radius = sqrt(X^2 + Y^2 + Z^2);
  }
 
 void loop(){
@@ -18,16 +19,108 @@ void loop(){
 
 void sphere(){
      int X,Y,Z,T,r;
-     char theta,fai;
+     char theta,fai,time;
+     const time = T;
      X = r * sin(theta) * cos(fai);
      Y = r * sin(theta) * sin(fai);
-     Z = r * cos(fai);
-     T = r * sin(theta);
+     Z = r * cos(theta);
+     r ^ 2 = x ^ 2 + y ^ 2 + z ^ 2;
 
-  const Length = 1; 
-  const r = Length;
+    const Length = 1; 
+    const r = Length;
     Coordinate(0,0,1),//A
     Coordinate(acos(-1/3), 0,0), // B
     Coordinate(0, -acos(-1/3),0), // C
     Coordinate(0,0,acos(-1/3)) // D
 ;}
+
+inline void mincowskispacetime(){
+     matrix11 = 
+            {{1,0,0,0},
+             {0,1,0,0},
+             {0,0,1,0},
+             {0,0,0,1}}
+    matrix12 = 
+            {{2,0,0,0},
+             {0,-2,0,0},
+             {0,0,2,0},
+             {0,0,0,-2}}
+    matrix13 = 
+            {{-3,0,0,0},
+             {0,3,0,0},
+             {0,0,3,},
+             {0,0,0,-3}}
+    matrix14 = 
+            {{-4,0,0,0},
+             {0,-4,0,0},
+             {0,0,-4,0},
+             {0,0,0,-4}}
+ 
+    matrix21 = 
+            {{5,0,0,5},
+             {0,5,5,0},
+             {0,5,5,0},
+             {5,0,0,5}}
+    matrix22 = 
+            {{6,6,6,6},
+             {6,0,0,6},
+             {6,0,0,6},
+             {6,6,6,6}}
+    matrix23 = 
+            {{0,7,7,0},
+             {7,0,0,7},
+             {7,0,0,7},
+             {0,7,7,0}}
+    matrix24 = 
+            {{8,8,8,8},
+             {0,0,0,0},
+             {8,8,8,8},
+             {0,0,0,0}}
+
+    matrix31 = 
+            {{0,0,0,0},
+             {9,9,9,9},
+             {0,0,0,0},
+             {9,9,9,9}}
+    matrix32 = 
+            {{10,0,0,0},
+             {0,5,0,0},
+             {0,0,10,0},
+             {0,0,0,5}}
+    matrix33 = 
+            {{11,0,0,0},
+             {0,10,0,0},
+             {0,0,11,0},
+             {0,0,0,10}
+    matrix34 = 
+            {{0,6,0,0},
+             {12,0,0,0},
+             {0,0,0,12},
+             {0,0,6,0}}
+
+    matrix41 = 
+            {{13,14,13,14},
+             {14,13,14,13},
+             {13,14,13,14},
+             {14,13,14,13}}
+    matrix42 = 
+            {{15,15,15,15},
+             {15,15,15,15},
+             {15,15,15,15},
+             {15,15,15,15}}
+    matrix43 = 
+            {{3,1,3,0},
+             {0,5,1,5},
+             {3,0,3,1},
+             {1,5,0,5}}
+    matrix44 = 
+            {{4,0,4,4},
+             {0,4,4,4},
+             {4,4,4,0},
+             {4,4,0,4}}
+
+;}
+
+extern void manifold(orbifold){
+    
+}
