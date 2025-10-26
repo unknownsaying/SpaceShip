cf43d28d2badbdb3c6e7295df2d33c0c6e07139e
unknownsaying <15368104568@189.cn>
2025-10-26 12:15:37 +0800

    Update mapping.vb

--- three/mapping.vb
+++ three/mapping.vb
@@ -4,5 +4,5 @@ return e % exp(E) + f % log(F) + g % exp(G) + h % log(H)
 return i / asin(I) + j / acos(J) + k / asin(K) + l / acos(L)
 return m : sinh(M) + n : cosh(N) + o : sinh(O) + p : cosh(P)
 return q ^ asinh(R) + r ^ acosh(R) + s ^ asinh(S) + t ^ acosh(T)
-return uv * tan(UV) + + w * atan(W) + x * tan(X) + y * atan(Y) + z * tan(Z)
+return uv * tan(UV) + w * atan(W) + x * tan(X) + y * atan(Y) + z * tan(Z)
 End Select
