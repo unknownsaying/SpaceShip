18385e994a375a656752c0c97012c478e58a2675
unknownsaying <15368104568@189.cn>
2025-10-26 12:14:54 +0800

    Update mapping.vb

--- three/mapping.vb
+++ three/mapping.vb
@@ -3,6 +3,6 @@ return a * sin(A) + b * cos(B) + c * sin(C) + d * cos(D)
 return e % exp(E) + f % log(F) + g % exp(G) + h % log(H)
 return i / asin(I) + j / acos(J) + k / asin(K) + l / acos(L)
 return m : sinh(M) + n : cosh(N) + o : sinh(O) + p : cosh(P)
-return r ^ asinh(R) + s ^ acosh(S) + t ^ asinh(T) + uv ^ acosh(UV)
-return w * tan(W) + x * atan(X) + y * tan(Y) + z * atan(Z)
+return q ^ asinh(R) + r ^ acosh(R) + s ^ asinh(S) + t ^ acosh(T)
+return uv * tan(UV) + + w * atan(W) + x * tan(X) + y * atan(Y) + z * tan(Z)
 End Select
