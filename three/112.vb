520c535e3c3bd6a9f40b397207c42cc5d2520042
unknownsaying <15368104568@189.cn>
2025-10-27 16:51:04 +0800

    Update 112.vb

--- three/112.vb
+++ three/112.vb
@@ -417,15 +417,16 @@ console.WriteLine(square3,square4,square5,square6,square7,square8,square9)
 const DET = det
 const tri = tri - antitri
 const ONE = one + oppoone
-const Square = square - antisquare
-const Pyramid = pyramid / antipyramid
-
 const DETtri = DET * tri
 const triDET = tri * DET
-const ONEsquare = ONE * square
-const squareONE = square / ONE
+
+const Square = square - antisquare
+const Pyramid = pyramid - antipyramid
+const ONEsquare = ONE % square
+const squareONE = square % ONE
 const oppoPyramid = antipyramid / pyramid
 const dimPyramid = oppopyramid / Pyramid
+
 Sub SUM
      ["DET/tri"+"tri/DET" == "DETtri"+"triDET"]
      ["DETtri"+"ONEsquare" == "squareONE"+"triDET"]
